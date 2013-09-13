a492411f94b33b1a0178ebb2b65978bfd2ce389d
Marius Butuc <marius.butuc@gmail.com>
2013-09-13 11:27:38 +0200

    Extracting a long SQL query in a private method

--- app/models/stats/tag_cloud.rb
+++ app/models/stats/tag_cloud.rb
@@ -35,19 +35,8 @@ class TagCloud
     @tags_divisor = ((max - @tags_min) / levels) + 1
 
     # Get the tag cloud for all tags for actions
-    query = "SELECT tags.id, tags.name AS name, count(*) AS count"
-    query << " FROM taggings, tags, todos"
-    query << " WHERE tags.id = tag_id"
-    query << " AND todos.user_id=? "
-    query << " AND taggings.taggable_type='Todo' "
-    query << " AND taggings.taggable_id=todos.id "
-    query << " AND (todos.created_at > ? OR "
-    query << "      todos.completed_at > ?) "
-    query << " GROUP BY tags.id, tags.name"
-    query << " ORDER BY count DESC, name"
-    query << " LIMIT 100"
     @tags_for_cloud_90days = Tag.find_by_sql(
-      [query, current_user.id, @cut_off_3months, @cut_off_3months]
+      [sql_90days, current_user.id, @cut_off_3months, @cut_off_3months]
     ).sort_by { |tag| tag.name.downcase }
 
     max_90days, @tags_min_90days = 0, 0
@@ -57,5 +46,25 @@ class TagCloud
     }
 
     @tags_divisor_90days = ((max_90days - @tags_min_90days) / levels) + 1
+  end
+
+private
+
+  def sql_90days
+    sql = <<-SQL
+      SELECT tags.id, tags.name AS name, count(*) AS count
+        FROM taggings, tags, todos
+        WHERE tags.id = tag_id
+        AND todos.user_id=?
+        AND taggings.taggable_type='Todo'
+        AND taggings.taggable_id=todos.id
+        AND (todos.created_at > ? OR
+             todos.completed_at > ?)
+        GROUP BY tags.id, tags.name
+        ORDER BY count DESC, name
+        LIMIT 100
+     SQL
+
+     sql.squish
   end
 end
